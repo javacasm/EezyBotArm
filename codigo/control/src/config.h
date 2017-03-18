aafd3c579877645e1ac347de4ba112c9ee6c0ddd
javacasm <javacasm@gmail.com>
2017-03-18 09:28:50 +0100

    v0.9

--- codigo/control/src/config.h
+++ codigo/control/src/config.h
@@ -3,10 +3,10 @@
 // betweenn 1000 and 2000. 1500 in the center
 #define MinGrip 1200
 #define MaxGrip 1800
-#define MinRot 1000
-#define MaxRot 2000
-#define MinRight 1200
-#define MaxRight 1800
+#define MinRot 500
+#define MaxRot 2200
+#define MinRight 500
+#define MaxRight 2500
 #define MinLeft 1200
 #define MaxLeft 1800
 
@@ -14,12 +14,15 @@
 #define MinControl 0
 #define MaxControl 1023
 
-#define PinServGrip 3
-#define PinServRot  4
-#define PinServLeft 5
-#define PinServRight 6
+
+#define PinServRot  7
+#define PinServLeft 6
+#define PinServRight 5
+#define PinServGrip 4
 
 #define PinControlGrip A0
+//#define PinControlRot A1
 #define PinControlRot A1
+// #define PinControlLeft A2
 #define PinControlLeft A2
 #define PinControlRight A3
