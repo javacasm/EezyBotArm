8cf8859f62026c04c912681fa22ad6fec30bf19b
javacasm <javacasm@gmail.com>
2017-03-18 12:33:22 +0100

    calibración

--- codigo/control/src/config.h
+++ codigo/control/src/config.h
@@ -1,14 +1,14 @@
 
 // using microseconds https://www.arduino.cc/en/Reference/ServoWriteMicroseconds
 // betweenn 1000 and 2000. 1500 in the center
-#define MinGrip 1200
-#define MaxGrip 1800
+#define MinGrip 500
+#define MaxGrip 2200
 #define MinRot 500
 #define MaxRot 2200
 #define MinRight 500
 #define MaxRight 2500
-#define MinLeft 1200
-#define MaxLeft 1800
+#define MinLeft 1500
+#define MaxLeft 2000
 
 
 #define MinControl 0
@@ -20,7 +20,7 @@
 #define PinServRight 5
 #define PinServGrip 4
 
-#define PinControlGrip A0
+#define PinControlGrip A5
 //#define PinControlRot A1
 #define PinControlRot A1
 // #define PinControlLeft A2
